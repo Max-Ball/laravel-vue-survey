9c209983a344b57b803148925f51f2fec1e0aeea
Max Ball <max.ball3388@gmail.com>
2022-10-18 09:12:20 -0600

    added form

--- vue/src/router/index.js
+++ vue/src/router/index.js
@@ -1,31 +1,63 @@
 import { createRouter, createWebHistory } from "vue-router"
 // @ts-ignore
-import Dashboard from '../views/Dashboard.vue'
+import DefaultLayout from '../components/DefaultLayout.vue'
+// @ts-ignore
+import AuthLayout from '../components/AuthLayout.vue'
 // @ts-ignore
 import Login from '../views/Login.vue'
 // @ts-ignore
 import Register from '../views/Register.vue'
+// @ts-ignore
+import Dashboard from '../views/Dashboard.vue'
+// @ts-ignore
+import Surveys from '../views/Surveys.vue'
+import store from "../store"
 
 const routes = [
   {
-    path: '/login',
-    name: 'Login',
-    component: Login
-  },
-  {
     path: '/',
-    name: 'Dashboard',
-    component: Dashboard
+    redirect: '/dashboard',
+    component: DefaultLayout,
+    meta: { requiresAuth: true },
+    children: [
+      { path: '/dashboard', name: 'Dashboard', component: Dashboard },
+      { path: '/surveys', name: 'Surveys', component: Surveys }
+    ]
   },
   {
-    path: '/register',
-    name: 'Register',
-    component: Register
-  },
+    path: '/auth',
+    redirect: '/login',
+    name: 'Auth',
+    meta: { isGuest: true },
+    component: AuthLayout,
+    children: [
+      {
+        path: '/login',
+        name: 'Login',
+        component: Login
+      },
+      {
+        path: '/register',
+        name: 'Register',
+        component: Register
+      }
+    ]
+
+  }
 ];
 const router = createRouter({
   history: createWebHistory(),
   routes
+})
+
+router.beforeEach((to, from, next) => {
+  if (to.meta.requiresAuth && !store.state.user.token) {
+    next({ name: 'Login' })
+  } else if (store.state.user.token && to.meta.isGuest) {
+    next({ name: 'Dashboard' })
+  } else {
+    next()
+  }
 })
 
 export default router
