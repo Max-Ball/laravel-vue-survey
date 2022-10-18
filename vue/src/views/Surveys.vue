bf085c9a11c6f1a2ef508b140c45c565c4a2bf98
Max Ball <max.ball3388@gmail.com>
2022-10-18 10:58:48 -0600

    components done

--- vue/src/views/Surveys.vue
+++ vue/src/views/Surveys.vue
@@ -1,20 +1,12 @@
 <template>
-  <header class="bg-white shadow">
-    <div class="mx-auto max-w-7xl py-6 px-4 sm:px-6 lg:px-8">
-      <h1 class="text-3xl font-bold tracking-tight text-gray-900">Surveys</h1>
-    </div>
-  </header>
-  <main>
-    <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
-      <!-- Replace with your content -->
-      <div class="px-4 py-6 sm:px-0">
-        CONTENT GOES HERE
-      </div>
-      <!-- /End replace -->
-    </div>
-  </main>
+  <PageComponent title="Surveys">
+    Content Goes Here
+  </PageComponent>
 </template>
 
-<script>
+
+
+<script setup>
+import PageComponent from "../components/PageComponent.vue"
 
 </script>
