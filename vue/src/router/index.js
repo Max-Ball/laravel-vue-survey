3f161fcf9f2b797fe29df3470075d54705c2662c
Max Ball <max.ball3388@gmail.com>
2022-10-19 11:58:59 -0600

    survey/question front-end

--- vue/src/router/index.js
+++ vue/src/router/index.js
@@ -11,6 +11,8 @@ import Register from '../views/Register.vue'
 import Dashboard from '../views/Dashboard.vue'
 // @ts-ignore
 import Surveys from '../views/Surveys.vue'
+// @ts-ignore
+import SurveyView from '../views/SurveyView.vue'
 import store from "../store"
 
 const routes = [
@@ -21,7 +23,9 @@ const routes = [
     meta: { requiresAuth: true },
     children: [
       { path: '/dashboard', name: 'Dashboard', component: Dashboard },
-      { path: '/surveys', name: 'Surveys', component: Surveys }
+      { path: '/surveys', name: 'Surveys', component: Surveys },
+      { path: '/surveys/create', name: 'SurveyCreate', component: SurveyView },
+      { path: '/surveys/:id', name: 'SurveyView', component: SurveyView }
     ]
   },
   {
--- vue/src/store/index.js
+++ vue/src/store/index.js
@@ -1,13 +1,104 @@
 import { createStore } from 'vuex'
 import axiosClient from '../axios'
 
+const tmpSurveys = [
+  {
+    id: 100,
+    title: "Max Survey",
+    slug: "my survey me max",
+    status: "draft",
+    image: "https://www.thiscatdoesnotexist.com",
+    description: "It's a survey by me",
+    created_at: "2022-10-28 17:10:00",
+    updated_at: "2022-10-28 17:10:00",
+    expire_date: "2022-11-28 17:10:00",
+    questions: [
+      {
+        id: 1,
+        type: "select",
+        question: "From which country are you?",
+        description: null,
+        data: {
+          options: [
+            { uuid: "f8af96f2-1d80-4632-9e9e-b560670e52ea", text: "USA" },
+            { uuid: "d8af43s2-1d80-4632-9e9e-g564070e52fc", text: "China" }
+          ]
+        }
+      },
+      {
+        id: 2,
+        type: "checkbox",
+        question: "Which language do you speak?",
+        description: "what a dumb question",
+        data: {
+          options: [
+            {
+              uuid: "x8af46f2-1d80-4632-9e9e-b560670e52ea",
+              text: "English"
+            },
+            {
+              uuid: "t4af46f2-1d80-4632-3x4z-b560670e52ea",
+              text: "Chinese"
+            }
+          ]
+        }
+      }
+    ]
+  },
+  {
+    id: 200,
+    title: "Another Survey",
+    slug: "another survey by me max",
+    status: "draft",
+    image: "https://www.thiscatdoesnotexist.com",
+    description: "It's another survey by me",
+    created_at: "2022-10-28 17:10:00",
+    updated_at: "2022-10-28 17:10:00",
+    expire_date: "2022-11-28 17:10:00",
+    questions: [
+      {
+        id: 3,
+        type: "select",
+        question: "From which city are you?",
+        description: null,
+        data: {
+          options: [
+            { uuid: "s8af96f2-1d80-4632-9e9e-b560670e52ea", text: "Boise" },
+            { uuid: "p8af43s2-1d80-4632-9e9e-g564070e52fc", text: "Portland" }
+          ]
+        }
+      },
+      {
+        id: 4,
+        type: "checkbox",
+        question: "Which language do you write?",
+        description: "what a dumb question",
+        data: {
+          options: [
+            {
+              uuid: "t8af46f2-1d80-4632-9e9e-b560670e52ea",
+              text: "English"
+            },
+            {
+              uuid: "m4af46f2-1d80-4632-3x4z-b560670e52ea",
+              text: "Mandarin"
+            }
+          ]
+        }
+      }
+    ]
+  }
+]
+
 const store = createStore(
   {
     state: {
       user: {
         data: {},
         token: sessionStorage.getItem('TOKEN')
-      }
+      },
+      surveys: [...tmpSurveys],
+      questionTypes: ["text", "select", "radio", "checkbox", "textarea"],
     },
     getters: {},
     actions: {
